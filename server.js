5413fde59073f973c896fe79220ae25d25bed9a1
gabepatino2002-ctrl <gabepatino2002@gmail.com>
2025-08-19 02:49:14 -0400

    server.js

--- server.js
+++ server.js
@@ -72,3 +72,7 @@ app.get("/callback", async (req, res) => {
 app.listen(3000, () => {
   console.log("✅ Server running on port 3000");
 });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
